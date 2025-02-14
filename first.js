ea04ce10c02b659d502de250ffc0f6cab42e8e34
SumitSinghShahi <167546911+SumitSinghShahi@users.noreply.github.com>
2025-02-14 19:12:21 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,85 @@
+ /*const product = {
+    title : " ball pen ",
+    rating : 4,
+    offer : 5,
+    price : 250
+}
+console.log(product)*/
+// let a=5;
+// let b=7;
+
+// console.log("a =", a+=4); 
+
+//comparison operator 
+
+// let a = 5 ;
+// let b = 2;
+
+// console.log("5 == 2" , a != b);
+
+//equal to and type
+ 
+// let a = 5;
+// let b = "5";
+
+// console.log (a !== b);
+
+//logical operators 
+
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a>b; //true
+// let cond2 = a === 5 ; //true
+// console.log ("cond1 ||f cond2 = ", cond1 || cond2);
+
+//conditional statement 
+ 
+// let age = 15;
+// if(age >18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you couldn't ");
+// }
+
+// let mode  = "dark";
+// let color;
+// if(mode === "dark" ){
+//     color = "black";
+// }
+// if(mode === "light" ){
+//     color = "white";
+// }
+// console.log(color);
+
+// let n=5;
+// if(n%2==0){
+//     console.log("number is even");
+// }
+// else {
+//     console.log("number is odd");
+// }
+
+//  let mode = "bl" ;
+//  let color;
+
+//  if (mode === "dark" ){
+//     color = "black";
+//  }
+//  else if (mode === "blue"){
+//       color = "neela"
+//  }
+//  else if (mode === "green"){
+//     color = "green"
+//  }
+//  else {
+//     color = "white"
+//  }
+
+//  console.log(color);
+
+// let age = 13 ;
+// let result = age >= 18 ? " adult " : " not adult ";
+// console.log(result);
+alert("hello@!")
